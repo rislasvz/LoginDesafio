42fdea482c03ae710366d3d7e3c142bdb8be71e5
rislasvz10 <rislasvz.10@gmail.com>
2022-11-16 08:34:23 -0600

    new proyect for login

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const db = require("./src/database/ConnectionMongoose");
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+const server = app.listen(PORT, () => console.log(`Server up on port ${PORT}`))
+const database = app.listen(db)
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
--- src/database/ConnectionMongoose.js
+++ src/database/ConnectionMongoose.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
-const MONGO_URL = "mongodb+srv://rislas:OK1u6iJzOBMYwxG2@cluster0.hud9yvm.mongodb.net/STORE?retryWrites=true&w=majority"
+const MONGO_URL = "mongodb+srv://rislas:Bk23ALt^8^pK@coderhouse.av7aeok.mongodb.net/?retryWrites=true&w=majority"
 
 const db = async () => {
   await mongoose.connect(MONGO_URL)
   .then(() => console.log("Conectados a MongoDB 🤖"))
   .catch((error) => console.error("❌ Error al conectarse a MongoDB", error));
 };
+
+module.exports = db
